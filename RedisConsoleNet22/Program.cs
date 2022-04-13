38e04de43a5d495c73b547c2a645ab3233b7f292
gustavocriado <gustavoliracriado30@gmail.com>
2022-04-12 23:22:01 -0300

    subindo

---
+++ RedisConsoleNet22/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace RedisConsoleNet22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coon = ConnectionMultiplexer.Connect("20.225.241.127");
+
+            var db = coon.GetDatabase();
+
+            var sub = coon.GetSubscriber();
+
+            sub.Subscribe("perguntas").OnMessage(m => {
+                string sla = m.Message;
+                Console.WriteLine(sla);
+                string resposta = Console.ReadLine();
+                db.HashSet(sla.Substring(0, sla.IndexOf(":")), "Marotos", resposta);
+            });
+            Task.Delay(2000000000).Wait();
+        }
+    }
+}
